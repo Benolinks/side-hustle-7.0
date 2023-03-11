544aab9e2af927e5cbe1bf4b0261c734dac50263
Benolinks <benolinks1@gmail.com>
2023-03-11 17:36:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$desc = "<center><h2>side hustle internship 7.0 Assignment on my name</h2> </center>";
+
+
+$my_name = "<center> <h1>Eze Benjamin .C</h1> </center>";
+
+
+
+echo $desc;
+
+echo $my_name;
+
+
+
+
+
+
+
+?>
